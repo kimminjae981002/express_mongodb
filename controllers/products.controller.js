e09d92df51fabb0aab83b4a75149d64fae7fd8de
minjae Kim <cmg981548@gmail.com>
2024-04-06 15:25:45 +0900

    feat: [main] mongoose crud

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -9,6 +9,53 @@ async function createProduct(req, res, next) {
   }
 }
 
+async function getProducts(req, res, next) {
+  try {
+    const products = await productModel.find();
+    res.status(201).json(products);
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function getProduct(req, res, next) {
+  try {
+    const { productId } = req.params;
+    const product = await productModel.findById(productId);
+    res.status(20).json(product);
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function updateProduct(req, res, next) {
+  try {
+    const { productId } = req.params;
+    const updateProduct = await productModel.findByIdAndUpdate(
+      productId,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updateProduct);
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function daleteProduct(req, res, next) {
+  try {
+    const { productId } = req.params;
+    const deleteeProduct = await productModel.findByIdAndDelete(productId);
+    res.status(200).json(deleteeProduct);
+  } catch (error) {
+    next(error);
+  }
+}
+
 module.exports = {
   createProduct,
+  getProducts,
+  getProduct,
+  updateProduct,
+  daleteProduct,
 };
--- routes/products.router.js
+++ routes/products.router.js
@@ -2,6 +2,10 @@ const express = require("express");
 const productsRouter = express.Router();
 const productsController = require("../controllers/products.controller");
 
-productsRouter.post("/product", productsController.createProduct);
+productsRouter.post("/", productsController.createProduct);
+productsRouter.get("/", productsController.getProducts);
+productsRouter.get("/:productId", productsController.getProduct);
+productsRouter.patch("/:productId", productsController.updateProduct);
+productsRouter.delete("/:productId", productsController.daleteProduct);
 
 module.exports = productsRouter;
